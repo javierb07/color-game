aa3bd6db7ff0826ed6234e3122558445a5aa3ad4
Javier Belmonte <43077436+javierb07@users.noreply.github.com>
2022-07-19 19:20:33 -0300

    Fix vulnerabilities

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
 # Guessing Color Game
+
 A web based color guessing game designed to help practice RGB colors. Based of and expanded from a small project on Colt Steele's Bootcamp on Udemy.
 
 ## Additions from the base code
+
 * Hardest mode
 * Scores
 * Instructions on hot to play
 * Database
 * Highscore table
+* Sound
 
 ## Ideas to add:
+
 * Animations
-* Sound
 * UI improvements
 * Mobile friendly version
 
 ### Play the game:
+
 https://color-game-07.herokuapp.com/
--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 'use strict';
 
-var express        = require("express"),
-    app            = express(),
-    mongoose       = require("mongoose"),
-    Player         = require("./models/player"),
+var express = require("express"),
+    app = express(),
+    mongoose = require("mongoose"),
+    Player = require("./models/player"),
     methodOverride = require("method-override"),
-    bodyParser     = require('body-parser');
+    bodyParser = require('body-parser');
 
 // App configuration
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 // Automatically parse request body as form data.
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride('_method'));
 
 // Set up default mongoose connection
 const host = process.env.HOST || "mongodb://localhost:27017/color-game";
-mongoose.connect(host,{ useNewUrlParser: true ,useUnifiedTopology: true}, function(err){
-    if (err){
-        console.log("Conection error to database")
+mongoose.connect(host, { useNewUrlParser: true, useUnifiedTopology: true }, function (err) {
+    if (err) {
+        console.log("Connection error to database")
     } else {
         console.log("Connected to database")
     }
@@ -31,29 +31,29 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // Landing route
-app.get('/', function(req,res){
+app.get('/', function (req, res) {
     res.render("index");
 });
 
 // Game route
-app.post('/color-game', function(req,res){
+app.post('/color-game', function (req, res) {
     var newPlayer = {
         playername: req.body.playername,
         score: 0
     }
-    Player.create(newPlayer, function(err, newlyCreated){
-        if(err){
+    Player.create(newPlayer, function (err, newlyCreated) {
+        if (err) {
             console.log(err);
-            res.render("error", {error: err});
+            res.render("error", { error: err });
         } else {
             // Redirect to play the game
-            res.render("game", {player: newlyCreated});
-        } 
+            res.render("game", { player: newlyCreated });
+        }
     });
 });
 
 // Update database on game end
-app.put('/color-game', function(req,res){
+app.put('/color-game', function (req, res) {
     const obj = JSON.parse(JSON.stringify(req.body));
     var player = {
         name: obj["data[playerName]"],
@@ -61,61 +61,61 @@ app.put('/color-game', function(req,res){
         difficulty: obj["data[difficulty]"],
     }
     let id = obj["data[id]"];
-    Player.findByIdAndUpdate(id, player, function(err, updated){
-        if(err){
+    Player.findByIdAndUpdate(id, player, function (err, updated) {
+        if (err) {
             console.log(err);
-            res.render("error", {error: err});
+            res.render("error", { error: err });
         } else {
             // Redirect to play the game
             res.redirect("/scores");
-        } 
+        }
     });
 });
 
 // Highscores route
-app.get('/scores', function(req,res){
-    Player.find({}, function(err, players){
-        if(err){
+app.get('/scores', function (req, res) {
+    Player.find({}, function (err, players) {
+        if (err) {
             req.flash("error", "Problem finding players in database");
             console.log(err);
-            res.render("error", {error: err});
+            res.render("error", { error: err });
         } else {
-            let playersEasy = players.filter(obj => {return obj.difficulty === "easy"});
+            let playersEasy = players.filter(obj => { return obj.difficulty === "easy" });
             playersEasy.sort((a, b) => { return b.score - a.score; });
-            let playersNormal = players.filter(obj => {return obj.difficulty === "normal"});
+            let playersNormal = players.filter(obj => { return obj.difficulty === "normal" });
             playersNormal.sort((a, b) => { return b.score - a.score; });
-            let playersHard = players.filter(obj => {return obj.difficulty === "hard"});
+            let playersHard = players.filter(obj => { return obj.difficulty === "hard" });
             playersHard.sort((a, b) => { return b.score - a.score; });
-            res.render("scores", {playersEasy: playersEasy, playersNormal: playersNormal, playersHard: playersHard});
+            res.render("scores", { playersEasy: playersEasy, playersNormal: playersNormal, playersHard: playersHard });
         }
     })
 });
-app.put('/scores', function(req,res){
-    Player.find({}, function(err, players){
-        if(err){
+app.put('/scores', function (req, res) {
+    Player.find({}, function (err, players) {
+        if (err) {
             req.flash("error", "Problem finding players in database");
             console.log(err);
-            res.render("error", {error: err});
+            res.render("error", { error: err });
         } else {
-            let playersEasy = players.filter(obj => {return obj.difficulty === "easy"});
+            let playersEasy = players.filter(obj => { return obj.difficulty === "easy" });
             playersEasy.sort((a, b) => { return b.score - a.score; });
-            let playersNormal = players.filter(obj => {return obj.difficulty === "normal"});
+            let playersNormal = players.filter(obj => { return obj.difficulty === "normal" });
             playersNormal.sort((a, b) => { return b.score - a.score; });
-            let playersHard = players.filter(obj => {return obj.difficulty === "hard"});
+            let playersHard = players.filter(obj => { return obj.difficulty === "hard" });
             playersHard.sort((a, b) => { return b.score - a.score; });
-            res.render("scores", {playersEasy: playersEasy, playersNormal: playersNormal, playersHard: playersHard});
+            res.render("scores", { playersEasy: playersEasy, playersNormal: playersNormal, playersHard: playersHard });
         }
     })
 });
 
 // Catch all other routes
-app.get('/*', function(req,res){
+app.get('/*', function (req, res) {
     res.redirect("/");
 });
 const port = process.env.PORT || 80;
 
-app.listen(port, function(err){
-    if (err){
+app.listen(port, function (err) {
+    if (err) {
         console.log("Something went wrong.");
     } else {
         console.log("Server is running.");
